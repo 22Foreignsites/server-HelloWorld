c204954ec7380b2b15838eb8e43946e27a0d40d8
Fred Pearson IV <Fredpearsoniv@gmail.com>
2025-10-15 01:02:11 -0400

    starting hello world

---
+++ app.js
@@ -0,0 +1,10 @@
+var http = require("http");
+const port = process.env.PORT || 3001;
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World! Yes sir");
+    //you have to run http://localhost:3001/ for 
+  })
+  .listen(port);
