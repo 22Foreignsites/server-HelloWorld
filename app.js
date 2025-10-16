408b229554d31cdceeb6d5b3e84cffce7087e1f1
Fred Pearson IV <Fredpearsoniv@gmail.com>
2025-10-16 10:49:29 -0400

    adding a very orignal statment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World! Yes sir");
+    res.end("Hello World! Yes sir, Yes sir, Your reading this and from the suburbs, 22 foreign on the beat, and you know he goes to USC");
     //you have to run http://localhost:3001/ for 
   })
   .listen(port);
